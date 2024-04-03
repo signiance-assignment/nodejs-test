15ecec5d0e4a49d45018e0decf4e644eed7328b6
root <root@ip-172-31-46-229.ap-south-1.compute.internal>
2024-04-03 11:12:19 +0000

    firstcommit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const hostname = '172.31.46.229'; // Your server ip address
+const port = 3000;
+
+const version = '1.0.0';
+
+app.get('/', (req, res) => {
+    // set response content    
+        res.send(`<html>
+                    <body>
+                        <h1>Hello World</h1>
+                    </body>
+                   </html>`);
+ 
+
+})
+
+app.listen(port, () => {
+    console.log(`Hello world`);
+})
