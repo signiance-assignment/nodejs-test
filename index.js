5d5e0b20a7e15a7b31c7cc67a0238bc37fb232f2
lokesh devops <97424924+loky1997@users.noreply.github.com>
2024-04-10 14:43:53 +0530

    Update index.js
    changes the code to hello world

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     // set response content    
         res.send(`<html>
                     <body>
-                        <h1>Hello Lokesh welocome to the Devops World</h1>
+                        <h1>Hello World</h1>
                     </body>
                    </html>`);
  
